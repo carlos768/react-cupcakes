369e8796bb02aca5be9ce8a46993878dc3a84b99
Carlos Ramirez <carlosramirez.768@gmail.com>
2022-10-11 22:31:59 -0300

    update nav and cupcakes card styles

--- src/components/sections/Header.js
+++ src/components/sections/Header.js
@@ -3,14 +3,14 @@ import { NavLink } from "react-router-dom"
 const Header = () =>{
 return(
     <header>
-        <nav className="d-flex flex-wrap justify-content-between align-items-center py-3 mx-5 mb-4">
+        <nav className="d-flex flex-wrap  justify-content-center justify-content-sm-between align-items-center py-3 mx-5 mb-4">
       
       <a href="/" className="navbar-brand d-flex align-items-center">
         <img height="50px" src="https://cdn-icons-png.flaticon.com/512/174/174394.png" alt="logo"/>
         <span className="fs-4 ">React cupcakes</span>
       </a>
 
-      <ul className="nav nav-pills">
+      <ul className="nav nav-pills flex-nowrap">
             <li className="nav-item"><NavLink className="nav-link" to="/" exact>Inicio</NavLink></li>
             <li className="nav-item"><NavLink className="nav-link" to="/cupcakes" exact>Cupcakes</NavLink></li>
             <li className="nav-item"><NavLink className="nav-link" to="/nosotros" exact>Nosotros</NavLink></li>
